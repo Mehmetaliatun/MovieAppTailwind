730a26519766e783808d0c5a1bdd43080b521375
Atun <mali1996ua@gmail.com>
2022-11-07 21:51:09 +0300

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,10 +2,26 @@ import React, { useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
 
 const Register = () => {
+  //! ayri stateler
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  //! birlestirilmis stateler
+  // const [info, setInfo] = useState({
+  //   firstName:"",
+  //   lastName:"",
+  //   password:"",
+  //   email:"",
+  // })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password);
+  };
+
+  // const handleChange=(e)=> setInfo
+
   return (
     <div className="flex justify-center">
       <div className="form-image hidden md:block">
@@ -19,7 +35,10 @@ const Register = () => {
         <div
           className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
         >
-          <form className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20">
+          <form
+            className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20"
+            onSubmit={handleSubmit}
+          >
             <h2 className="text-[#ff4b45] text-2xl font-[500] text-center tracking-[0.1em]">
               Sign Up
             </h2>
