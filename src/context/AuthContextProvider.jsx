a22fa2269fb9b5a1718f768bdb55be5ea33e00a3
Atun <mali1996ua@gmail.com>
2022-11-08 22:56:04 +0300

    baseUrl

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { userObserver } from "../auth/firebase";
 
 export const AuthContext = createContext();
