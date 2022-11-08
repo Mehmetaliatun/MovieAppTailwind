888922efb3558b3d1201a3c41c127d0f9eca3a84
Atun <mali1996ua@gmail.com>
2022-11-08 20:37:05 +0300

    auth context- navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../assets/icons/avatar.png";
 import { logOut } from "../auth/firebase";
-
+import { AuthContext } from "../context/AuthContextProvider";
 const Navbar = () => {
-  const currentUser = { displayName: "mehmet ali" };
+  const { currentUser } = useContext(AuthContext);
+  // const currentUser = { displayName: "mehmet ali" };
   // const currentUser = false;
   return (
     <>
--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,10 +1,11 @@
 import React, { createContext, useEffect, useState } from "react";
 import { userObserver } from "../auth/firebase";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(false);
+  console.log(currentUser);
   useEffect(() => {
     userObserver(setCurrentUser);
   }, []);
